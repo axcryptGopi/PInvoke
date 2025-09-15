b0d5c05943e56bb9f022c18c3c485befe15783f4
Gopinath <Gopinath.b@axcrypt.net>
2025-09-15 14:54:16 +0530

    First commit

---
+++ PInvoke/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace PInvoke
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "PInvoke" };
+        }
+    }
+}
---
+++ PInvoke/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PInvoke
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
